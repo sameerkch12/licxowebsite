8a018b5b6f1a99baf81f8841af5bef4e31bbdb0d
Sameerkch12 <sameer.sc62@gmail.com>
2025-11-28 19:51:57 +0530

    homepage chnage

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -42,14 +42,14 @@ export default function App() {
         <div className="max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8 py-2"></div>
       </header>
 
-      <main className="flex-grow w-full py-8 sm:py-10 lg:py-12">
+      <main className="">
         <div className="max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="overflow-hidden">
             <div className="grid grid-cols-1 md:grid-cols-2 gap-0">
 
               {/* Search Section */}
               <div className="p-6 sm:p-8 lg:p-12 flex flex-col justify-center">
-                <h2 className="text-2xl md:text-2xl font-bold text-indigo-700 mb-1 dark:text-indigo-400">
+                <h2 className=" text-lg md:text-2xl font-bold text-indigo-700 mb-1 dark:text-indigo-400">
                   <span className="text-gray-900 dark:text-gray-50">
                     Find Rooms and PG
                   </span> at Best Prices
@@ -69,7 +69,7 @@ export default function App() {
                 </form>
 
                 {/* Top Destinations */}
-                <div className="mt-10 pt-6 border-t border-gray-100 dark:border-gray-800">
+                <div className=" pt-6 border-t border-gray-100 dark:border-gray-800">
                   <h3 className="text-base font-semibold text-gray-900 mb-4 flex items-center justify-between dark:text-gray-50">
                     <span className="flex items-center">
                       <TrendingUp className="w-5 h-5 text-indigo-500 mr-2 dark:text-indigo-400" />
